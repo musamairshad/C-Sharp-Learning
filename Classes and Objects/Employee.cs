e4db5153c89d8184f6e8ddc27678f97cece14139
Muhammad Usama <imusamairshad@gmail.com>
2023-08-30 22:13:04 +0500

    Employee.cs file is modified.

--- Classes and Objects/Employee.cs
+++ Classes and Objects/Employee.cs
@@ -8,5 +8,20 @@ namespace Classes_and_Objects
 {
     internal class Employee
     {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Department { get; set; }
+
+        public void login() 
+        {
+            // Add logic here......
+            Console.WriteLine(FullName + " has logged in at " + DateTime.Now);
+        }
+
+        public void logout()
+        {
+            // Add logic here......
+            Console.WriteLine(FullName + " has logged out at " + DateTime.Now);
+        }
     }
 }
