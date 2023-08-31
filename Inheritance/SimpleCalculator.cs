7c7ae78b4ad3f303a48a7a151e676e8af6b4b0b1
Muhammad Usama <imusamairshad@gmail.com>
2023-08-31 18:27:35 +0500

    ScientificCalculator.cs is added.

---
+++ Inheritance/SimpleCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class SimpleCalculator
+    {
+        public double Add(int num1, int num2) 
+        {
+            return num1 + num2;
+        }
+
+        public double Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public double Divide(int num1, int num2) 
+        {
+            return num1 / num2;
+        }
+    }
+}
