e3531a42b56fe545cf01b8df05e8f28acf3bbf9e
Muhammad Usama <imusamairshad@gmail.com>
2023-08-31 18:25:54 +0500

    Files are modified.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleCalculator simpleCalculator = new SimpleCalculator();
+            ScientificCalculator scientificCalculator = new ScientificCalculator();
+            double result = simpleCalculator.Add(2, 3);
+            double result2 = scientificCalculator.Add(10, 10);
+            double result3 = scientificCalculator.Sin(30);
+            Console.WriteLine(result3);
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+            Console.ReadLine();
+        }
+    }
+}
