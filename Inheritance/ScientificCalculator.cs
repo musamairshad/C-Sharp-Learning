dbbcb73225f6cf391ef11a4cb6607df0dac30958
Muhammad Usama <imusamairshad@gmail.com>
2023-08-31 18:26:37 +0500

    ScientificCalculator.cs is added.

---
+++ Inheritance/ScientificCalculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class ScientificCalculator : SimpleCalculator
+    {
+        public double Sin(double rad)
+        {
+            return Math.Sin(rad);
+        }
+    }
+}
