c0f8cc60c7de48d3ae658bc74874bd21721bd885
Muhammad Usama <imusamairshad@gmail.com>
2023-09-03 18:49:45 +0500

    Animal.cs is added.

---
+++ Method Overriding/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overriding
+{
+    public class Animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Animal eats......");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void eat() // Method overriding. 
+        {
+            base.eat(); // Calling the eat() method of the base class.
+            Console.WriteLine("Cat eats meat...");
+        }
+    }
+}
