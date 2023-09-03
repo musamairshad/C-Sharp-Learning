90f6784024a62248383766d2f0b7630a7782b179
Muhammad Usama <imusamairshad@gmail.com>
2023-09-03 22:05:57 +0500

    Program.cs is added.

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Bank bank = new Bank();
+            bank.setBalance(-1000);
+            Console.WriteLine(bank.getBalance());
+            Console.ReadLine();
+            */
+
+            Bank2 ubl = new Bank2();
+            ubl.Balance = -1000;
+            Console.WriteLine(ubl.Balance);
+            Console.ReadLine();
+        }
+    }
+}
