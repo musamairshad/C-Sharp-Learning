bc4867ee24a0f412fd99472a608d705e74cc6b41
Muhammad Usama <imusamairshad@gmail.com>
2023-09-03 18:50:44 +0500

    SimpleCalculator.cs is modified.

--- Inheritance/SimpleCalculator.cs
+++ Inheritance/SimpleCalculator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +14,21 @@ namespace Inheritance
             return num1 + num2;
         }
 
+        public double Add(int num1, int num2, int num3)
+        {
+            return num1 + num2 + num3;
+        }
+
+        public double Add(List<int> listOfInts)
+        {
+            double sum = 0;
+            foreach (var item in listOfInts)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+
         public double Subtract(int num1, int num2)
         {
             return num1 - num2;
@@ -23,7 +39,12 @@ namespace Inheritance
             return num1 * num2;
         }
 
-        public double Divide(int num1, int num2) 
+        public double Divide(int num1, int num2) // Method overloading.
+        {
+            return num1 / num2;
+        }
+
+        public double Divide(double num1, double num2)
         {
             return num1 / num2;
         }
