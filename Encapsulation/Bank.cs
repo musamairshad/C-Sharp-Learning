1c530f3033f8be1752e928624d75648ef0218a9d
Muhammad Usama <imusamairshad@gmail.com>
2023-09-03 22:06:15 +0500

    Bank.cs is added.

---
+++ Encapsulation/Bank.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Bank
+    {
+        private double balance;
+
+        public double getBalance() // Accessor.
+        {
+            return balance; 
+        }
+
+        public void setBalance(double balance) // Mutator.
+        {
+            if (balance < 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Balance cannot be negative!");
+            } else
+            {
+                this.balance = balance;
+            }
+        }
+    }
+
+    public class Bank2
+    {
+        private double balance;
+
+        public double Balance {
+            get
+            {
+                return balance;
+            } 
+            set 
+            {
+                if(value < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Balance cannot be negative!");
+                } 
+                else
+                {
+                    this.balance = value;
+                }
+            }
+        }
+    }
+}
