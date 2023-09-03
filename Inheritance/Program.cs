1cfde690a8b83fc6b2f513dcce053e9f3804db02
Muhammad Usama <imusamairshad@gmail.com>
2023-09-03 18:50:23 +0500

    Program.cs is modified.

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -12,6 +12,13 @@ namespace Inheritance
         {
             SimpleCalculator simpleCalculator = new SimpleCalculator();
             ScientificCalculator scientificCalculator = new ScientificCalculator();
+            double result = scientificCalculator.Divide(1, 8);
+            double result2 = scientificCalculator.Divide(3.1, 2.1);
+            double result3 = scientificCalculator.Add(2, 4, 6);
+            // double result4 = scientificCalculator.Add([1, 2, 3, 4]);
+            Console.WriteLine(result3);
+            Console.ReadLine();
+            /*
             double result = simpleCalculator.Add(2, 3);
             double result2 = scientificCalculator.Add(10, 10);
             double result3 = scientificCalculator.Sin(30);
@@ -19,6 +26,7 @@ namespace Inheritance
             Console.WriteLine(result);
             Console.WriteLine(result2);
             Console.ReadLine();
+            */
         }
     }
 }
