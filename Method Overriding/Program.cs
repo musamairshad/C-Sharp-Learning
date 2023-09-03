50a6f52c2dee59c2a1a81f9f28f8c6964fae04c9
Muhammad Usama <imusamairshad@gmail.com>
2023-09-03 18:50:03 +0500

    Program.cs is added.

---
+++ Method Overriding/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overriding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat cat = new Cat();
+            cat.eat();
+            Console.ReadLine();
+        }
+    }
+}
